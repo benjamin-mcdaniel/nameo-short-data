510675b398e64dd26423cdac5a13c4752cc3b478
benjamin-mcdaniel <benjamin.f.mcdaniel@gmail.com>
2023-05-21 21:53:07 -0700

    some basic domain checking

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import whois
+import datetime
 
 def is_domain_registered(domain):
     try:
@@ -6,3 +7,22 @@ def is_domain_registered(domain):
         return True  # Domain is registered
     except whois.parser.PywhoisError:
         return False  # Domain is not registered
+
+def generate_domain_names(names_file, tlds_file):
+    domain_names = []
+    
+    # Read names from the names file
+    with open(names_file, 'r') as file:
+        names = file.read().splitlines()
+    
+    # Read TLDs from the TLD file
+    with open(tlds_file, 'r') as file:
+        tlds = file.read().splitlines()
+    
+    # Generate domain names by combining names and TLDs
+    for name in names:
+        for tld in tlds:
+            domain_names.append(name + '.' + tld)
+    
+    return domain_names
+
--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 from functions import *
-domain = "example.com"
-is_registered = is_domain_registered(domain)
-print(is_registered)
+
+
+def main():
+    names_file = 'names'  # Path to the names file
+    tlds_file = 'tlds'    # Path to the TLDs file
+    
+    domain_names = generate_domain_names(names_file, tlds_file)
+    
+    results = []
+    
+    # Check availability for each domain name
+    for domain in domain_names:
+        is_registered = is_domain_registered(domain)
+        results.append((domain, is_registered))
+    
+    # Save results to a CSV file with the current date in the filename
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
+    filename = f'{date}_domains.csv'
+    
+    with open(filename, 'w') as file:
+        file.write('Domain,Status\n')  # Header row
+        
+        for result in results:
+            domain, is_registered = result
+            status = 'Registered' if is_registered else 'Not Registered'
+            file.write(f'{domain},{status}\n')
+    
+    print(f"Results saved to {filename}.")
+
+if __name__ == '__main__':
+    main()

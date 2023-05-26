4732f6b8cef3f661a4549d6811844e181c5a08f0
benjamin-mcdaniel <benjamin.f.mcdaniel@gmail.com>
2023-05-26 00:23:45 -0700

    fixing things

--- functions.py
+++ functions.py
@@ -1,43 +1,108 @@
-def collect_domain_data(domain):
-    functions = [function1, function2, function3, function4, function5]
+import csv
+import boto3
+import requests
 
-    for func in functions:
-        result = func(domain)
-        if not result:
-            write_data(False, domain)
-            return
+# AWS S3 bucket information
+bucket_name = 'nameo-data'
+folder_with_files = 'domains/'
+tld_file = 'tld/tld.csv'
+location_file = 'location'
 
-    write_data(True, domain)
+# Initialize AWS S3 client
+s3 = boto3.client('s3')
 
-def function1(domain):
-    # Logic for function 1
-    return True
+def read_file_from_s3(bucket, key):
+    response = s3.get_object(Bucket=bucket, Key=key)
+    return response['Body'].read().decode('utf-8')
 
-def function2(domain):
-    # Logic for function 2
-    return True
+def write_file_to_s3(bucket, key, data):
+    s3.put_object(Body=data, Bucket=bucket, Key=key)
 
-def function3(domain):
-    # Logic for function 3
-    return True
+def read_location():
+    try:
+        location = int(read_file_from_s3(bucket_name, location_file))
+    except:
+        location = 0
+    return location
 
-def function4(domain):
-    # Logic for function 4
-    return True
+def write_location(location):
+    write_file_to_s3(bucket_name, location_file, str(location))
 
-def function5(domain):
-    # Logic for function 5
-    return True
+def combine_domains_tlds():
+    # Read the TLD file
+    tld_data = read_file_from_s3(bucket_name, tld_file)
+    tlds = list(csv.reader(tld_data.splitlines()))
 
-def write_data(success, domain):
-    if success:
-        # Logic for writing data when all functions succeed
-        print(f"Writing data for successful domain: {domain}")
-    else:
-        # Logic for writing data when any function fails
-        print(f"Writing data for failed domain: {domain}")
+    # Get the starting location
+    location = read_location()
 
-# Example usage
-domain_to_process = "example.com"
-collect_domain_data(domain_to_process)
+    # Read the domain files
+    domain_files = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_with_files)['Contents']
+    domain_files = [obj['Key'] for obj in domain_files]
 
+    for i in range(location, len(domain_files)):
+        domain_file = domain_files[i]
+        domain_data = read_file_from_s3(bucket_name, domain_file)
+        domains = domain_data.split('\n')
+
+        for domain in domains:
+            for tld in tlds:
+                if domain:
+                    domain_tld = f"{domain}.{tld[0]}"
+                    domain_checker(domain_tld, tld[1])
+
+        location = i + 1
+        write_location(location)
+
+
+
+
+def domain_checker(domain, rdap_url=None):
+    tld = domain.split('.')[-1]
+
+    # Perform name server check
+    # Replace this with your own name server check logic
+    ns_check_result = perform_name_server_check(domain)
+
+    # Perform WHOIS lookup
+    # Replace this with your own WHOIS lookup logic
+    whois_result = perform_whois_lookup(domain)
+
+    # Perform RDAP request
+    if rdap_url is None or not rdap_url.startswith("http"):
+        rdap_url = f"https://root.rdap.org/domain/{tld}"
+    rdap_result = perform_rdap_request(domain, rdap_url)
+
+    # Determine if the domain is registered based on the results
+    is_registered = ns_check_result or whois_result or rdap_result
+
+    # Pass the response to the recorder function
+    recorder(domain, is_registered)
+
+
+def perform_name_server_check(domain):
+    # Replace this with your name server check logic
+    # Return True or False based on the check result
+    return False
+
+
+def perform_whois_lookup(domain):
+    # Replace this with your WHOIS lookup logic
+    # Return True or False based on the lookup result
+    return False
+
+
+def perform_rdap_request(domain, rdap_url):
+    try:
+        response = requests.get(rdap_url)
+        # Replace this with your RDAP request parsing logic
+        # Return True or False based on the RDAP response
+        return True
+    except requests.exceptions.RequestException:
+        return False
+
+
+def recorder(domain, is_registered):
+    # Replace this with your recording logic
+    # Print or save the domain and is_registered value
+    print(f"Domain: {domain}, Is Registered: {is_registered}")
--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-import boto3
-bucket_name = 'your-s3-bucket-name'
+from functions import *
 
-def load_data(bucket_name, prefix):
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket_name)
-    management_string = ""
+# Main function
 
-    try:
-        for obj in bucket.objects.filter(Prefix=prefix):
-            if obj.key.endswith('.txt'):
-                response = obj.get()
-                content = response['Body'].read().decode('utf-8')
-                management_string += content
-    except Exception as e:
-        print(f"Error: Failed to load files from S3 bucket: {e}")
-        return None
+def main():
+    combine_domains_tlds()
 
-    return management_string
-
-def process_files(bucket_name):
-    name_data = load_data(bucket_name, 'name/')
-    tld_data = load_data(bucket_name, 'tld/')
-
-    if name_data is None or tld_data is None:
-        print("Error: Missing files in the S3 bucket.")
-        return
-
-    # Perform your desired operations with the management string
-    print("Loaded name data:")
-    print(name_data)
-    print("Loaded TLD data:")
-    print(tld_data)
-    
-process_files(bucket_name)
+if __name__ == "__main__":
+    main()
